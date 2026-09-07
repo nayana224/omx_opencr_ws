eea4826d5725e258f09b59f9d08ca80213d2aac7
inpyo_dev <inpyoi1304@gmail.com>
2026-09-07 14:06:01 +0900

    docs: refresh student sketch examples

--- Omx_Manual.ino
+++ Omx_Manual.ino
@@ -15,22 +15,29 @@ void setup()
 void loop()
 {
   /*
-    이 파일은 함수 실습용 스케치입니다.
-    아래 예시를 하나씩 주석 해제해서 정기구학/역기구학 동작을 관찰하세요.
+    학생 실습용 예시입니다.
+    필요한 함수 한 줄씩 주석을 해제해서 동작을 관찰하세요.
 
+    // 상태 읽기
     readJoint();
     readTCP();
 
+    // Joint 제어
     moveHome();
     moveJointAbs(0, 20, -20, 0, 2.0);
     moveJointRel(0, 10, 0, 0, 1.5);
 
+    // TCP 제어
     moveTCPAbs(0.20, 0.00, 0.10, 2.0);
     moveTCPRel(0.00, 0.00, 0.03, 1.5);
 
+    // 자세 / Gripper
     keepHorizontal();
     setPitch(15);
-    setGripper(true);
+    openGripper();
+    closeGripper();
+
+    // 한 번만 실행하려면 마지막에 사용
+    while (1);
   */
 }
-
