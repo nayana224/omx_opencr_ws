c0faca198f0acda89c7bc874ce911c9a3e4efbfa
inpyo_dev <inpyoi1304@gmail.com>
2026-09-07 18:42:01 +0900

    style: normalize sketch comments

--- Omx_Manual.ino
+++ Omx_Manual.ino
@@ -13,26 +13,26 @@ void setup()
 void loop()
 {
   /*
-    // 상태 읽기
+    // 상태 확인
     readJoint();
     readTCP();
 
-    // Joint 제어
+    // Joint 제어 [degree]
     moveHome();
     moveJointAbs(0, 20, -20, 0);
     moveJointRel(0, 10, 0, 0);
 
-    // TCP 직선 이동
+    // TCP 직선 이동 [m]
     moveTCPAbs(0.20, 0.00, 0.10);
     moveTCPRel(0.00, 0.00, 0.03);
 
-    // 자세 / 그리퍼
+    // End-effector 자세 / Gripper 제어
     keepHorizontal();
     setPitch(15);
     openGripper();
     closeGripper();
 
-    // 한 번만 실행
+    // 단일 시퀀스 종료
     while (1);
   */
 }
